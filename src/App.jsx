cc37ce346dde0ba67206bea3899af3946e65ce33
Lugacity01 <yinkaabeebadesina@gmail.com>
2024-10-22 07:23:23 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
               element: <Home/>,
             },
             {
-              path: '/about-us',
+              path: '/about',
               element: <About/>,
             },
             {
